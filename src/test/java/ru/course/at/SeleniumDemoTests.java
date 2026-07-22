e1009501930ab2affbdf12b856ef1ac0216ba785
nikolaykrukov <kolysik00@mail.ru>
2026-07-22 20:46:52 +1000

    homework

---
+++ src/test/java/ru/course/at/SeleniumDemoTests.java
@@ -0,0 +1,44 @@
+package ru.course.at;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumDemoTests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://www.bing.com/");
+    }
+
+    @AfterEach
+    public void tearDown() { driver.quit(); }
+
+    @Test
+    public void search() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
+        assertEquals(input, searchPageField.getAttribute("value"));
+    }
+
+}
